03fccc317052690046c70153cc5310539fa2094b
timothytosh <tim.tosh@petroskills.com>
2014-02-07 13:42:37 -0600

    update with more faux content and additions.

--- index.php
+++ index.php
@@ -37,18 +37,19 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#">Project name</a>
+          <a class="navbar-brand" href="#">Prime Dev Games</a>
         </div>
         <div class="navbar-collapse collapse">
           <ul class="nav navbar-nav">
             <li class="active"><a href="#">Home</a></li>
-            <li><a href="#">About</a></li>
+            <li><a href="#">Games</a></li>
+              <li><a href="#">About</a></li>
             <li><a href="#">Contact</a></li>
             <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Stuff <b class="caret"></b></a>
               <ul class="dropdown-menu">
-                <li><a href="#">Action</a></li>
-                <li><a href="#">Another action</a></li>
+                <li><a href="#">More Stuff</a></li>
+                <li><a href="#">Extra Stuff</a></li>
                 <li><a href="#">Something else here</a></li>
                 <li class="divider"></li>
                 <li class="dropdown-header">Nav header</li>
@@ -76,22 +77,22 @@
   </ol>
   <div class="carousel-inner">
     <div class="item active">
-      <img data-src="holder.js/900x500/auto/#777:#7a7a7a/text:First slide" src="assets/images/underwater-carousel-1.jpg" alt="First slide">
+      <img data-src="holder.js/900x500/auto/#777:#7a7a7a/text:First slide" src="assets/images/underwater-carousel-4.jpg" alt="Bubble Rescue">
       <div class="container">
         <div class="carousel-caption">
-          <h1>Example headline.</h1>
-          <p>Note: If you're viewing this page via a <code>file://</code> URL, the "next" and "previous" Glyphicon buttons on the left and right might not load/display properly due to web browser security rules.</p>
-          <p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
+          <h1>Bubble Rescue</h1>
+          <p>Our first game to be released is an excellent underwater puzzle game. Rescue the main characters Gerard, Crunchy and MO on a fun filled trip to collecting items and having fun.</p>
+          <p><a class="btn btn-lg btn-primary" href="#" role="button">Download today</a></p>
         </div>
       </div>
     </div>
     <div class="item">
-      <img data-src="holder.js/900x500/auto/#666:#6a6a6a/text:Second slide" src="assets/images/forest-carousel-2.jpg" alt="Second slide">
+      <img data-src="holder.js/900x500/auto/#666:#6a6a6a/text:Second slide" src="assets/images/forest-carousel-2.jpg" alt="Bust a Nut">
       <div class="container">
         <div class="carousel-caption">
-          <h1>Another example headline.</h1>
-          <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-          <p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
+          <h1>Bust a Nut</h1>
+          <p>The follow up game to Bubble Rescue, Crunchy and his nuts have to find a way through the amazing puzzle levels. Along the way find some cool items to collect and share. </p>
+          <p><a class="btn btn-lg btn-primary" href="#" role="button">Beta download</a></p>
         </div>
       </div>
     </div>
@@ -99,9 +100,9 @@
       <img data-src="holder.js/900x500/auto/#555:#5a5a5a/text:Third slide" src="assets/images/rainbow-carousel-3.jpg" alt="Third slide">
       <div class="container">
         <div class="carousel-caption">
-          <h1>One more for good measure.</h1>
-          <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-          <p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
+          <h1>Coming Soon</h1>
+          <p>Coming soon many more puzzle games, RPG games and some stunning unexpected games to keep your senses alert!</p>
+          <p><a class="btn btn-lg btn-primary" href="#" role="button">Take a look</a></p>
         </div>
       </div>
     </div>
@@ -121,20 +122,20 @@
   <!-- Three columns of text below the carousel -->
   <div class="row">
     <div class="col-lg-4">
-      <img class="img-circle" data-src="holder.js/140x140" src="assets/images/item1.png" alt="Generic placeholder image">
-      <h2>Heading</h2>
+      <img class="img-circle" data-src="holder.js/140x140" src="assets/images/item1.png" alt="Crunchy">
+      <h2>Crunchy</h2>
       <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
       <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
     </div><!-- /.col-lg-4 -->
     <div class="col-lg-4">
-      <img class="img-circle" data-src="holder.js/140x140" src="assets/images/item2.png" alt="Generic placeholder image">
-      <h2>Heading</h2>
+      <img class="img-circle" data-src="holder.js/140x140" src="assets/images/item2.png" alt="Gerard">
+      <h2>Gerard</h2>
       <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
       <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
     </div><!-- /.col-lg-4 -->
     <div class="col-lg-4">
-      <img class="img-circle" data-src="holder.js/140x140" src="assets/images/item3.png" alt="Generic placeholder image">
-      <h2>Heading</h2>
+      <img class="img-circle" data-src="holder.js/140x140" src="assets/images/item3.png" alt="Mo">
+      <h2>Mo</h2>
       <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
       <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
     </div><!-- /.col-lg-4 -->
@@ -151,7 +152,7 @@
       <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
     </div>
     <div class="col-md-5">
-      <img class="featurette-image img-responsive" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+      <img class="featurette-image img-responsive" data-src="holder.js/500x500/auto" src="assets/images/feature1.png" alt="Generic placeholder image">
     </div>
   </div>
 
@@ -159,7 +160,7 @@
 
   <div class="row featurette">
     <div class="col-md-5">
-      <img class="featurette-image img-responsive" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+      <img class="featurette-image img-responsive" data-src="holder.js/500x500/auto" src="assets/images/feature2.png" alt="Generic placeholder image">
     </div>
     <div class="col-md-7">
       <h2 class="featurette-heading">Oh yeah, it's that good. <span class="text-muted">See for yourself.</span></h2>
@@ -175,7 +176,7 @@
       <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
     </div>
     <div class="col-md-5">
-      <img class="featurette-image img-responsive" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+      <img class="featurette-image img-responsive" data-src="holder.js/500x500/auto" src="assets/images/feature3.png" alt="Generic placeholder image">
     </div>
   </div>
 
@@ -187,7 +188,7 @@
   <!-- FOOTER -->
   <footer>
     <p class="pull-right"><a href="#">Back to top</a></p>
-    <p>&copy; 2014 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+    <p>Version 1.0.2 &middot; &copy; 2014 Prime Development, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
   </footer>
 
 </div><!-- /.container -->
